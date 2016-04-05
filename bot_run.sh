bc5af4f9fa954f2a6523f6b4cf8e96c4f8becf35
Martin Urbanec <martin.urbanec@wikimedia.cz>
2016-04-05 11:42:23 +0200

    Indef while, sleep 1 day

--- bot_run.sh
+++ bot_run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+while true; do;
 ~/orphan/to_delete_orphan.py;
 python ~/orphan/bot_process.py;
-mv result.json public/result.json
+mv result.json public/result.json;
+sleep 86400;
+done;
